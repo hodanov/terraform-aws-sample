05992edd92299975cd4dbb0d6c340dcd5b36d9dc
hodanov <1031hoda@gmail.com>
2020-03-17 23:14:13 +0900

    Add a module of IAM role.

---
+++ iam_role/main.tf
@@ -0,0 +1,41 @@
+variable "name" {}
+variable "policy" {}
+variable "identifier" {}
+
+// "aws_iam_role" defines IAM role.
+resource "aws_iam_role" "default" {
+  name               = var.name
+  assume_role_poricy = data.aws_iam_policy_document.assume_role.json
+}
+
+// "aws_iam_policy_document" defines an assume policy.
+// This is used by "aws_iam_role".
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = [var.identifier]
+    }
+  }
+}
+
+// "aws_iam_policy" defines IAM policy.
+resource "aws_iam_policy" "default" {
+  name   = var.name
+  policy = var.policy
+}
+
+// "aws_iam_role_policy_attachment" attach policy to role.
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
+}
+
+output "iam_role_arn" {
+  value = aws_iam_role.default.arn
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.default.name
+}
